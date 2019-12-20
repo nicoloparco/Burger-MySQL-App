a84288ee9b5b937dc86d08a648bd5516445bf9fe
Nico Loparco <nicoloparco75@gmail.com>
2019-12-20 18:09:52 -0500

    fixed connection error

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "docker",
-    database: "burger_db"
+    database: "burgers_db"
 });
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-USE burgers_db
+USE burger_db
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Hamburger", false), ("Cheeseburger", false), ("Bacon Burger", false), ("Royale with Cheese", false)
