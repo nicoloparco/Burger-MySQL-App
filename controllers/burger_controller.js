95e4870317850802e5dded22d7e0d01212987faa
Nico Loparco <nicoloparco75@gmail.com>
2019-12-20 18:02:25 -0500

    completed controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,3 +1,34 @@
 const express = require("express")
 const router = express.Router()
 const burger = require("../models/burger.js")
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data){
+        var hbrsObj = {
+            burgers: data
+        };
+        res.render("index", hbrsObj)
+    })
+});
+
+router.post("/api/burgers", function(req, res){
+    burger.insertOne(
+        ["burger_name", "devoured"], 
+        [req.body.burger_name, req.body.devoured],
+        function(result){
+            res.json(result)
+        });
+});
+
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id
+    burger.updateOne({devoured: req.body.devoured}, condition, function(result){
+        if(result, changedRows === 0){
+            return res.status(404).end();
+        } else{
+            res.status(200)
+        }
+    })
+})
+
+module.exports = router;
